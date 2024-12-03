c9df169020f8e22fee97235409007a5feebdd31a
lpm0831 <lpm0831@naver.com>
2024-12-03 09:03:05 +0900

    Hilt 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -56,4 +56,9 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
 
     implementation(libs.timber)
+
+    // hilt
+    implementation(libs.hilt)
+    kapt (libs.dagger.hilt.compiler)
+
 }
